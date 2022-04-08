4381d0d542430231db99ed121ca5a3e102e6edf5
sanuv9683 <sanuv9683@gmail.com>
2022-04-08 11:22:17 +0530

    Content Updated

--- 02_HttpRequest/src/MyServlet.java
+++ 02_HttpRequest/src/MyServlet.java
@@ -16,6 +16,30 @@ public class MyServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
-        writer.write("Hello Iam from HTTP Request Module");
+        writer.write("Request Received from GET Method");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Request Received from PUT Method");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Request Received from POST Method");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Request Received from DELETE Method");;
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Request Received from OPTIONS Method");;
     }
 }
