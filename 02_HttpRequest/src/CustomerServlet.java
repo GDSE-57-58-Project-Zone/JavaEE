d933dca326eae12a2e5ed66088160027dc04bbee
sanuv9683 <sanuv9683@gmail.com>
2022-04-08 11:14:23 +0530

    Contet Updated

--- 02_HttpRequest/src/CustomerServlet.java
+++ 02_HttpRequest/src/CustomerServlet.java
@@ -10,18 +10,42 @@ import java.io.PrintWriter;
  * @author : Sanu Vithanage
  * @since : 0.1.0
  **/
+/*if someone send a request to localhost:8080/absd/customer url this class will handle that
+ request and process a response according to the method which requested from the client*/
+
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
 
+    /*Request methods*/
+    /*if customer receives get request this will execute*/
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         writer.write("Response from Customer Servlet Generated");
     }
 
+    /*if customer receives post request this fill execute*/
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter writer = resp.getWriter();
         writer.write("Post Method Called from Customer Servlet");
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("PUT Method called");
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Delete Method Called");
+    }
+
+    @Override
+    protected void doOptions(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("OPTION method invoked");
     }
 }
