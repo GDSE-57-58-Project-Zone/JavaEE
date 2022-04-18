25d6de350f46b5b54970639522f6970e9c223754
sanuv9683 <sanuv9683@gmail.com>
2022-04-18 11:01:51 +0530

    Content Updated on App One

--- 04_AppOne/src/CustomerServlet.java
+++ 04_AppOne/src/CustomerServlet.java
@@ -5,6 +5,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * @author : Sanu Vithanage
@@ -14,7 +18,26 @@ import java.io.PrintWriter;
 public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/company", "root", "sanu");
+            ResultSet rst = connection.prepareStatement("select * from Customer").executeQuery();
+            while (rst.next()) {
+                String id = rst.getString(1);
+                String name = rst.getString(2);
+                String address = rst.getString(3);
+                double salary = rst.getDouble(4);
+                System.out.println(id + " " + name + " " + address + " " + salary);
+            }
 
+            PrintWriter writer = resp.getWriter();
+            //writer.write(); //text //xml //html //json
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
 
     }
 }
