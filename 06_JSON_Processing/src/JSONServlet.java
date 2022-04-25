16ddd30032e9f4b6f66b874fae955effbab8643d
sanuv9683 <sanuv9683@gmail.com>
2022-04-25 11:43:25 +0530

    How to send a json object to the client

--- 06_JSON_Processing/src/JSONServlet.java
+++ 06_JSON_Processing/src/JSONServlet.java
@@ -1,5 +1,6 @@
 import javax.json.Json;
 import javax.json.JsonObject;
+import javax.json.JsonObjectBuilder;
 import javax.json.JsonReader;
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * @author : Sanu Vithanage
@@ -15,6 +17,24 @@ import java.io.IOException;
  **/
 @WebServlet(urlPatterns = "/json")
 public class JSONServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //id:C00-001, name:Ramal , address: Galle ,Salary : 10000
+
+        resp.setContentType("application/json");
+
+        JsonObjectBuilder objectB = Json.createObjectBuilder();
+        objectB.add("id","C001");
+        objectB.add("name","Ramal");
+        objectB.add("address","Galle");
+        objectB.add("salary",1000.00);
+        JsonObject build = objectB.build();
+
+        PrintWriter writer = resp.getWriter();
+        writer.print(build);
+
+    }
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
