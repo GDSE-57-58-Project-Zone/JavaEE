1121d58dfb748915b16674ba48572c806668e62b
sanuv9683 <sanuv9683@gmail.com>
2022-04-25 10:46:41 +0530

    Content Updated

--- 06_JSON_Processing/src/CustomerServlet.java
+++ 06_JSON_Processing/src/CustomerServlet.java
@@ -1,4 +1,5 @@
 import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -14,6 +15,14 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        System.out.println("Someone called DO PUT method");
+        System.out.println("Method Invoked");
+
+        ServletInputStream inputStream = req.getInputStream();
+
+        int read;
+        while ((read = inputStream.read()) != -1) {
+            System.out.print((char)read);
+        }
+
     }
 }
