61d4aea4ab0b2f89c56373e24ed2b28fa4eaa936
sanuv9683 <sanuv9683@gmail.com>
2022-05-02 10:42:39 +0530

    Aded how to read JSON array from a servlet

--- 06_JSON_Processing/src/JSONServlet.java
+++ 06_JSON_Processing/src/JSONServlet.java
@@ -1,6 +1,5 @@
 import javax.json.*;
 import javax.servlet.ServletException;
-import javax.servlet.ServletInputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -37,16 +36,16 @@ public class JSONServlet extends HttpServlet {
         JsonArrayBuilder arrayBuilder = Json.createArrayBuilder(); // create a json array
 
         JsonObjectBuilder objectB = Json.createObjectBuilder(); //Create json object one
-        objectB.add("id","C001");
-        objectB.add("name","Ramal");
-        objectB.add("address","Galle");
-        objectB.add("salary",1000.00);
+        objectB.add("id", "C001");
+        objectB.add("name", "Ramal");
+        objectB.add("address", "Galle");
+        objectB.add("salary", 1000.00);
 
         JsonObjectBuilder objectB2 = Json.createObjectBuilder();// Create json Object two
-        objectB2.add("id","C002");
-        objectB2.add("name","Sunimal");
-        objectB2.add("address","Panadura");
-        objectB2.add("salary",1000.00);
+        objectB2.add("id", "C002");
+        objectB2.add("name", "Sunimal");
+        objectB2.add("address", "Panadura");
+        objectB2.add("salary", 1000.00);
 
         arrayBuilder.add(objectB.build()); // add json object one to the json array
         arrayBuilder.add(objectB2.build()); // add json object two to the json array
@@ -74,7 +73,25 @@ public class JSONServlet extends HttpServlet {
         String id = jsonObject.getString("id");
         String name = jsonObject.getString("name");
         String address = jsonObject.getString("address");
-        System.out.println(id+" "+name+" "+address);
+        System.out.println(id + " " + name + " " + address);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        //read json array from a request
+        JsonReader reader = Json.createReader(req.getReader());
+        JsonArray jsonArray = reader.readArray();
+        for (JsonValue jsonValue : jsonArray) {
+
+            String customerID = jsonValue.asJsonObject().getString("id");
+            String customerName = jsonValue.asJsonObject().getString("name");
+            String customerAddress = jsonValue.asJsonObject().getString("address");
+            String customerSalary = jsonValue.asJsonObject().getString("salary");
+            System.out.println(customerID + " " + customerName + " " + customerAddress + " " + customerSalary);
+        }
 
 
     }
