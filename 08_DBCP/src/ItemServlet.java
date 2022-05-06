d2652263b28f7cc89e131bac32d881108faf76f2
sanuv9683 <sanuv9683@gmail.com>
2022-05-06 09:34:31 +0530

    Add another servlet and test servlet context

---
+++ 08_DBCP/src/ItemServlet.java
@@ -0,0 +1,39 @@
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * @author : Sanu Vithanage
+ * @since : 0.1.0
+ **/
+@WebServlet(urlPatterns = "/item")
+public class ItemServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource bds = (BasicDataSource) servletContext.getAttribute("bds");
+
+        try {
+            Connection connection = bds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("select * from Item");
+            ResultSet rst = pstm.executeQuery();
+            while (rst.next()) {
+                System.out.println(rst.getString(1));
+            }
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+    }
+}
