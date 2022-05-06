67705c65f159be79a464838d0f930c1614e69b99
sanuv9683 <sanuv9683@gmail.com>
2022-05-06 10:20:24 +0530

    Used ApllicationContext Listener to Implement the Pool

--- 08_DBCP/src/listeners/MyListener.java
+++ 08_DBCP/src/listeners/MyListener.java
@@ -1,5 +1,8 @@
 package listeners;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -14,6 +17,19 @@ public class MyListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         System.out.println("Context Initialized");
+        // This method invokes suddenly just after the creation of Servlet Context
+        //How to create the DBCP pool
+
+        BasicDataSource bds = new BasicDataSource();
+        bds.setDriverClassName("com.mysql.jdbc.Driver");
+        bds.setUrl("jdbc:mysql://localhost:3306/company");
+        bds.setUsername("root");
+        bds.setPassword("sanu");
+        bds.setMaxTotal(5); // how many connections
+        bds.setInitialSize(5); // how many connection we should initialize
+
+        ServletContext servletContext = servletContextEvent.getServletContext();// a common place for all servlet
+        servletContext.setAttribute("bds", bds); // store the pool inside the Servlet Context
     }
 
     @Override
--- 08_DBCP/src/servlet/CustomerServlet.java
+++ 08_DBCP/src/servlet/CustomerServlet.java
@@ -24,18 +24,20 @@ public class CustomerServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
-        //How to create the DBCP pool
-        BasicDataSource bds = new BasicDataSource();
-        bds.setDriverClassName("com.mysql.jdbc.Driver");
-        bds.setUrl("jdbc:mysql://localhost:3306/company");
-        bds.setUsername("root");
-        bds.setPassword("sanu");
-        bds.setMaxTotal(5); // how many connections
-        bds.setInitialSize(5); // how many connection we should initialize
+//        //How to create the DBCP pool
+//        BasicDataSource bds = new BasicDataSource();
+//        bds.setDriverClassName("com.mysql.jdbc.Driver");
+//        bds.setUrl("jdbc:mysql://localhost:3306/company");
+//        bds.setUsername("root");
+//        bds.setPassword("sanu");
+//        bds.setMaxTotal(5); // how many connections
+//        bds.setInitialSize(5); // how many connection we should initialize
+//
+//        ServletContext servletContext = req.getServletContext();// a common place for all servlet
+//        servletContext.setAttribute("bds", bds); // store the pool inside the Servlet Context
 
-        ServletContext servletContext = req.getServletContext();// a common place for all servlet
-        servletContext.setAttribute("bds", bds); // store the pool inside the Servlet Context
-
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource bds = (BasicDataSource) servletContext.getAttribute("bds");
         try {
             Connection connection = bds.getConnection();
             PreparedStatement pstm = connection.prepareStatement("select * from Customer");
