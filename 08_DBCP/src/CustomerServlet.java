8dac3ee8c2769f39832b16d08899d75dc01e7b4d
sanuv9683 <sanuv9683@gmail.com>
2022-05-06 09:25:24 +0530

    Accsess the Servlet Context from another method

--- 08_DBCP/src/CustomerServlet.java
+++ 08_DBCP/src/CustomerServlet.java
@@ -23,7 +23,7 @@ public class CustomerServlet extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
         //How to create the DBCP pool
-        BasicDataSource bds= new BasicDataSource();
+        BasicDataSource bds = new BasicDataSource();
         bds.setDriverClassName("com.mysql.jdbc.Driver");
         bds.setUrl("jdbc:mysql://localhost:3306/company");
         bds.setUsername("root");
@@ -32,7 +32,7 @@ public class CustomerServlet extends HttpServlet {
         bds.setInitialSize(5); // how many connection we should initialize
 
         ServletContext servletContext = req.getServletContext();// a common place for all servlet
-        servletContext.setAttribute("bds",bds); // store the pool inside the Servlet Context
+        servletContext.setAttribute("bds", bds); // store the pool inside the Servlet Context
 
 
         try {
@@ -52,6 +52,21 @@ public class CustomerServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        ServletContext servletContext = req.getServletContext();
+        BasicDataSource bds = (BasicDataSource) servletContext.getAttribute("bds");
 
+        try {
+            Connection connection = bds.getConnection();
+            PreparedStatement pstm = connection.prepareStatement("select * from Customer");
+            ResultSet rst = pstm.executeQuery();
+            while (rst.next()) {
+                String id = rst.getString(1);
+                System.out.println(id);
+            }
+
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
     }
 }
